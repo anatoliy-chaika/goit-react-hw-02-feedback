7bb742e6650b8eab15e3840faf3eb2d89511fac2
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2023-02-24 16:45:53 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './Feedback/FeedbackOptions'
+import { Section } from './Section/Section';
+import {Statistics} from "./Statistics/Statistics"
+import { GlobalStyles } from "./GlobalSyles.styled"
+import {Container} from "./Container.styled"
+
+
+
+export class App extends Component {
+  state = {
+    good:0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  handleIncrement = option => {
+    this.setState({[option]: this.state[option] + 1});
+  }
+  
+ countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+  
+  render() {
+    const { good, neutral, bad } = this.state;
+    const optionKeys = Object.keys(this.state)
+
+    return (<Container>
+      <GlobalStyles/>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={optionKeys}
+          onLeaveFeedback={this.handleIncrement}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback} positivePercentage={this.countPositiveFeedbackPercentage}></Statistics>
+      </Section>
+</Container>
+    );
+  }
+}
